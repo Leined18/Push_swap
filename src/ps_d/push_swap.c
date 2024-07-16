30818a96abb8e73c39303562c3f8d4856ded0c70
Leined18 <erdanielmarciano@hotmail.com>
2024-07-16 14:08:25 +0200

    update

--- src/ps_d/push_swap.c
+++ src/ps_d/push_swap.c
@@ -1,20 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danpalac <danpalac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 13:54:12 by danpalac          #+#    #+#             */
+/*   Updated: 2024/07/16 14:08:04 by danpalac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void	print_stack(t_stack *stack)
+void	print_stack(t_stack *a, t_stack *b)
 {
 	t_node	*current;
 
-	if (stack == NULL || stack->size == 0)
+	if (a)
 	{
-		ft_printf("Pila vacía\n");
-		return ;
+		ft_printf("Pila a: ");
+		current = a->head;
+		while (current != NULL)
+		{
+			ft_printf("(%d) ", current->num);
+			current = current->next;
+		}
 	}
-	current = stack->head;
-	ft_printf("Pila: ");
-	while (current != NULL)
+	ft_printf("\n");
+	current = NULL;
+	if (b)
 	{
-		ft_printf("(%d) ", current->num);
-		current = current->next;
+		ft_printf("Pila b: ");
+		current = b->head;
+		while (current != NULL)
+		{
+			ft_printf("(%d) ", current->num);
+			current = current->next;
+		}
 	}
 	ft_printf("\n");
 }
@@ -29,8 +51,7 @@ int	main(int ac, char **av)
 	ft_load_stack(&a, ac, av);
 	ft_stack_zero(&b, ac);
 	printf("Pila antes de ordenar:\n");
-	print_stack(a);
-	print_stack(b);
+	print_stack(a, b);
 	ft_printf("size a %d , b %d\n", a->size, b->size);
 	free_stack(a, a->size);
 	free_stack(b, b->size);
